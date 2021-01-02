6d1df47303e5a95c62b91a33d900b800d63ccf43
Luan Nascimento <72614631+Luanvbn@users.noreply.github.com>
2021-01-02 14:55:16 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet" media="screen">
+    <title>O Jogo</title>
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <h1>O Jogo</h1>
+        <h2>The Game</h2>
+        <h4>Perdedores</h4>
+        <p>Perdeu! Qual seu nome?</p>
+        <form action="" method="POST">
+            <label><input type="text" name="nome" placeholder="Seu nome" maxlength="10" id="nome"></label>
+            <input type="submit" value="Salvar" id="salvar" name="salvar">
+        </form>
+    </div>
+</body>
+
+</html>
+<?php
+
+$nome = $_POST['nome'];
+$con = mysqli_connect("localhost", "root", "", "banco_teste");
+$query="INSERT INTO nome VALUES('','$nome')";
+
+mysqli_query($con, $query);
+mysqli_close($con);
+?>
