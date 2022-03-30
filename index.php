8ced5565c1b68e3bc17e94b084d70069283f5fe7
Luanvbn <luanvbn@gmail.com>
2022-03-30 10:27:38 -0300

    Alterações de uso

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 $Nome = $_POST['nome'];
 
-$idprim = [1, 2, 3, 4, 5, 6, 7, 8];
-
+echo $Nome;
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
---
+++ teste.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+class Main
+{
+    public function __construct()
+    {
+        echo "Hello\n";
+    }
+
+    /**
+     *
+     * @param string $name
+     * @return void
+     */
+    public function teste(string $name): void
+    {
+    }
+}
